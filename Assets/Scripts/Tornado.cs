fed8ccf183c5a137aad84c99c9c31ca97f63af39
Menez sasha <s.menez@student.isartdigital.com>
2025-12-29 17:42:48 +0100

    add: particles

--- Assets/Scripts/Tornado.cs
+++ Assets/Scripts/Tornado.cs
@@ -1,33 +1,55 @@
+using System;
 using UnityEngine;
 using System.Collections.Generic;
 
 public class Tornado : MonoBehaviour
 {
+    public ParticleSystem tornadoParticles;
+    public List<Rigidbody> elements = new List<Rigidbody>();
     public float a = 0.5f; // Aspiration
     public float gamma = 20f; // Puissance de rotation
     public float nu = 0.1f; // Viscosité cinématique
-    public List<Rigidbody> elements = new List<Rigidbody>();
-    public GameObject tornadoVisual;
+    
+    private ParticleSystem.Particle[] particles;
+
+    private void Awake()
+    {
+        particles = new ParticleSystem.Particle[tornadoParticles.main.maxParticles];
+    }
 
     void FixedUpdate()
     {
         foreach (Rigidbody element in elements)
         {
-            Vector3 elementPos = element.position;
-            Vector3 transformPos = transform.position;
-            float z = elementPos.y - transformPos.y;
-            elementPos.y = 0;
-            transformPos.y = 0;
-            float dist = Vector3.Distance(elementPos, transformPos);
-            Vector3 Radial = (elementPos - transformPos).normalized;
-            Vector3 Tangential = Vector3.Cross(Vector3.up, Radial).normalized;
-            
-            float Vr = -a * dist;
-            float Vtheta = gamma / (2 * Mathf.PI * dist) * (1 - Mathf.Exp(- (a * dist * dist) / (2 * nu)));
-            float Vz = 2 * a * z;
-
-            Vector3 velocity = Vr * Radial + Vtheta * Tangential + Vz * Vector3.up;
-            element.linearVelocity = velocity;
+            element.linearVelocity = ApplyTornado(element.position);
         }
+    }
+
+    private void LateUpdate()
+    {
+        int numParticlesAlive = tornadoParticles.GetParticles(particles);
+
+        for (int i = 0; i < numParticlesAlive; i++)
+        {
+            particles[i].velocity = ApplyTornado(particles[i].position);
+        }
+        tornadoParticles.SetParticles(particles, numParticlesAlive);
+    }
+
+    private Vector3 ApplyTornado(Vector3 elementPos)
+    {
+        Vector3 transformPos = transform.position;
+        float z = elementPos.y - transformPos.y;
+        elementPos.y = 0;
+        transformPos.y = 0;
+        float dist = Vector3.Distance(elementPos, transformPos);
+        Vector3 Radial = (elementPos - transformPos).normalized;
+        Vector3 Tangential = Vector3.Cross(Vector3.up, Radial).normalized;
+            
+        float Vr = -a * dist;
+        float Vtheta = gamma / (2 * Mathf.PI * dist) * (1 - Mathf.Exp(- (a * dist * dist) / (2 * nu)));
+        float Vz = 2 * a * z;
+
+        return Vr * Radial + Vtheta * Tangential + Vz * Vector3.up;
     }
 }
