8963171f34105341cd001381fe54de1a14f737fe
Menez sasha <s.menez@student.isartdigital.com>
2025-12-30 01:12:30 +0100

    add: mass and debug flowField

--- Assets/Scripts/Tornado.cs
+++ Assets/Scripts/Tornado.cs
@@ -2,26 +2,61 @@ using System;
 using UnityEngine;
 using System.Collections.Generic;
 
+public enum GridType
+{
+    Square,
+    Cube
+}
+
 public class Tornado : MonoBehaviour
 {
     public ParticleSystem tornadoParticles;
     public List<Rigidbody> elements = new List<Rigidbody>();
+    public GameObject flowFieldPrefab;
+    [Header("Tornado Parameters")]
+    public float tornadoStrength = 20f;
     public float a = 0.5f; // Aspiration
     public float gamma = 20f; // Puissance de rotation
     public float nu = 0.1f; // Viscosité cinématique
-    
     private ParticleSystem.Particle[] particles;
+    [Header("FlowField Parameters")]
+    public GridType gridType = GridType.Cube;
+    public int size = 5;
+    public int resolution = 4;
+    private List<GameObject> flowField = new List<GameObject>();
+    private int lastResolution = 4;
 
     private void Awake()
     {
         particles = new ParticleSystem.Particle[tornadoParticles.main.maxParticles];
+        for (int i = 0; i < resolution; i++)
+        {
+            GameObject arrow = Instantiate(flowFieldPrefab);
+            arrow.SetActive(false);
+            flowField.Add(arrow);
+        }
+    }
+
+    private void Update()
+    {
+        if (lastResolution != resolution)
+        {
+            for (int i = 0; i < flowField.Count; i++)
+            {
+                flowField[i].SetActive(false);
+            }
+        }
+        lastResolution = resolution;
+        DrawFloawField();
     }
 
     void FixedUpdate()
     {
         foreach (Rigidbody element in elements)
         {
-            element.linearVelocity = ApplyTornado(element.position);
+            Vector3 fluidVelocity = ApplyTornado(element.position);
+            Vector3 relativeVelocity = fluidVelocity - element.linearVelocity;
+            element.AddForce(relativeVelocity * tornadoStrength, ForceMode.Force);
         }
     }
 
@@ -51,5 +86,53 @@ public class Tornado : MonoBehaviour
         float Vz = 2 * a * z;
 
         return Vr * Radial + Vtheta * Tangential + Vz * Vector3.up;
+    }
+
+    private void DrawFloawField()
+    {
+        DrawCubeField();
+    }
+
+    private void DrawCubeField()
+    {
+        int index = 0;
+        float halfsize = size / 2f;
+        float step = (resolution > 1) ? size / (float)(resolution - 1) : 0;
+        
+        for(int i = 0; i < resolution; i++)
+        {
+            for(int j = 0; j < resolution; j++)
+            {
+                for(int k = 0; k < resolution; k++)
+                {
+                    float posX = (i * step) - halfsize;
+                    float posY = (j * step);
+                    float posZ = (k * step) - halfsize;
+                    
+                    Vector3 relativePos = new Vector3(posX, posY, posZ);
+                    Vector3 worldPos = transform.position + relativePos;
+                    Vector3 flow = ApplyTornado(worldPos);
+                    
+                    GameObject arrow;
+                    if (index == flowField.Count)
+                    {
+                        arrow = Instantiate(flowFieldPrefab);
+                        flowField.Add(arrow);
+                    }
+                    else
+                        arrow = flowField[index];
+                    PlaceArrow(arrow, worldPos, flow);
+                    index++;
+                }
+            }
+        }
+    }
+    private void PlaceArrow(GameObject arrow, Vector3 position, Vector3 direction)
+    {
+        if(!arrow)
+            arrow = Instantiate(flowFieldPrefab);
+        arrow.transform.position = position;
+        arrow.transform.rotation = Quaternion.LookRotation(direction);
+        arrow.SetActive(true);
     }
 }
